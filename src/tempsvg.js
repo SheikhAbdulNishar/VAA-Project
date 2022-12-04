52f081ebfa83d366c5be89785679c69af45cb2c4
Carnage_007 <95233345+SheikhAbdulNishar@users.noreply.github.com>
2022-12-04 20:15:54 +0530

    Add files via upload

---
+++ src/tempsvg.js
@@ -0,0 +1,171 @@
+import * as React from "react"
+
+const SvgComponent = (props) => (
+  <svg
+    id="eMb52mNZDxs1"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 625 508"
+    shapeRendering="geometricPrecision"
+    textRendering="geometricPrecision"
+    {...props}
+  >
+    <rect
+      width={148.496}
+      height={39.124}
+      rx={19.562}
+      ry={19.562}
+      transform="rotate(-30 496.175 -51.05)"
+      fill="#7a69e7"
+    />
+    <rect
+      id="eMb52mNZDxs3"
+      width={148.496}
+      height={39.124}
+      rx={19.562}
+      ry={19.562}
+      transform="rotate(-150 290.682 69.554)"
+      fill="#7a69e7"
+    />
+    <g>
+      <rect
+        width={193}
+        height={168}
+        rx={0}
+        ry={0}
+        transform="matrix(1 0 0 -1 221 201)"
+        fill="#fff"
+      />
+      <ellipse
+        rx={112.865}
+        ry={168.651}
+        transform="translate(316.936 230.302)"
+        fill="#263092"
+      />
+      <ellipse
+        rx={96.5}
+        ry={84}
+        transform="translate(317.5 117)"
+        fill="#7a69e7"
+      />
+      <path
+        d="m315.492 346.677 51.008-32.694L385 230l-69.508 39.983L250 230l15.5 83.983 49.992 32.694Zm.016-16.194L250 230h135l-69.492 100.483Z"
+        clipRule="evenodd"
+        fill="#fffdfd"
+        fillRule="evenodd"
+      />
+      <path
+        d="m315.492 346.677.016-16.194m-.016 16.194 51.008-32.694L385 230m-69.508 116.677L265.5 313.983 250 230m135 0H250m135 0-69.492 100.483M385 230l-69.508 39.983L250 230m0 0 65.508 100.483"
+        fill="none"
+        stroke="#fff"
+        strokeWidth={5}
+      />
+      <rect
+        width={17}
+        height={58}
+        rx={0}
+        ry={0}
+        transform="translate(406 71)"
+        fill="#665696"
+      />
+      <rect
+        width={17}
+        height={58}
+        rx={0}
+        ry={0}
+        transform="translate(406 71)"
+        fillOpacity={0.2}
+      />
+      <rect
+        width={16}
+        height={57}
+        rx={0}
+        ry={0}
+        transform="translate(406.5 71.5)"
+        fill="none"
+        stroke="#000"
+      />
+      <ellipse rx={77} ry={62} transform="translate(317 117)" fill="#d9d9d9" />
+      <ellipse
+        rx={22.5}
+        ry={21}
+        transform="translate(284.5 108)"
+        fill="#263092"
+      />
+      <ellipse
+        rx={22.5}
+        ry={21}
+        transform="translate(348.5 108)"
+        fill="#263092"
+      />
+      <ellipse
+        rx={28}
+        ry={27.692}
+        transform="translate(283 114.692)"
+        fill="#7a69e7"
+      />
+      <rect
+        width={56}
+        height={32.308}
+        rx={0}
+        ry={0}
+        transform="translate(255 114.692)"
+        fill="#d9d9d9"
+      />
+      <ellipse
+        rx={22.474}
+        ry={20.385}
+        transform="translate(282.632 115.077)"
+        fill="#d9d9d9"
+      />
+      <ellipse
+        rx={28}
+        ry={26.769}
+        transform="translate(348 113.769)"
+        fill="#7a69e7"
+      />
+      <rect
+        width={56}
+        height={31.231}
+        rx={0}
+        ry={0}
+        transform="translate(320 113.769)"
+        fill="#d9d9d9"
+      />
+      <ellipse
+        rx={22.474}
+        ry={19.705}
+        transform="translate(347.632 114.141)"
+        fill="#d9d9d9"
+      />
+      <rect
+        width={17}
+        height={58}
+        rx={0}
+        ry={0}
+        transform="translate(213 71)"
+        fill="#665696"
+      />
+      <rect
+        width={17}
+        height={58}
+        rx={0}
+        ry={0}
+        transform="translate(213 71)"
+        fillOpacity={0.2}
+      />
+      <rect
+        width={16}
+        height={57}
+        rx={0}
+        ry={0}
+        transform="translate(213.5 71.5)"
+        fill="none"
+        stroke="#000"
+      />
+      <path d="M413 0h8v71h-8V0ZM213 0h8v71h-8V0Z" fill="#1d0202" />
+    </g>
+    <script />
+  </svg>
+)
+
+export default SvgComponent
